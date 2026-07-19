8cee8faebce4f6021ef44fd9ff879de9f74c5272
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2026-07-19 22:44:31 +0400

    useraccounts

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class UserAccount(AbstractUser):
+    USER_TYPE_CHOICES= {
+        'admin':'admin',
+        'student':'student',
+        'library':'library',
+        'teacher':'teacher',
+    }
+    phone = models.CharField(max_length=22,null=True,blank=True)
+    user_type=models.CharField(null=True,blank=True,choices=USER_TYPE_CHOICES)
+
+    
