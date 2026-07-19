3114068989931d133e5f0eb9307af8b4947eb715
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2026-07-19 20:17:33 +0400

    models and permissions

--- backend/Book/models.py
+++ backend/Book/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class books(models.Model):
+    book_name=models.CharField(max_length=100,null=True,blank=True)
+    publishdate=models.CharField(max_length=100,null=True,blank=True)
+    
