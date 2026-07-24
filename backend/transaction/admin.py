79fb0c3fea0b9d5229b3cc0d78222c37c6244975
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2026-07-24 22:54:18 +0400

    Transaction model

--- backend/transaction/admin.py
+++ backend/transaction/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Lended
 
 # Register your models here.
+admin.site.register(Lended)
--- backend/transaction/models.py
+++ backend/transaction/models.py
@@ -1,1 +1,14 @@
-
+from django.db import models
+from django.conf import settings
+from Book.models import Book
+class Lended(models.Model):
+    STATUS_CHOICES=[('Issued','issued'),('Returned','returned'),('Due','due'),('Lost','lost')]
+    book_name = models.ForeignKey(Book,null=True,blank=True,on_delete=models.CASCADE)
+    user= models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL,null=True,blank=True)
+    issued_officer = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL,null=True,blank=True,limit_choices_to={
+        'user_type':'library'
+    })
+    alloted_date=models.DateField(null=True,blank=True)
+    return_date=models.DateField(null=True,blank=True)
+    due_date=models.DateField(null=True,blank=True)
+    status=models.CharField(max_length=22,null=True,blank=True,choices=STATUS_CHOICES)
