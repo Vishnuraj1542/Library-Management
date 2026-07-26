3920ac4fc3b66bdc34cc909066a5cdde2fec290c
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2026-07-26 23:10:15 +0400

    forms

---
+++ backend/accounts/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import UserAccount
+
+class UsercreationForm(forms.ModelForm):
+    class Meta:
+        model=UserAccount
+        fields={'username','password','email','admission_number'}
