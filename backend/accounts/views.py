4a6d5b170a48bd80b501718039bd86ddd702dabc
vishnuraj1542 <vishnurajcheerankuzhi@gmail.com>
2026-08-04 23:18:31 +0400

    user creation'

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -4,17 +4,19 @@ from django import views
 from .forms import UsercreationForm
 
 # Create your views here.
+
 def user_creation(request):
-    if request.method == 'POST':
-       form= UsercreationForm(request.POST)
-       if form.is_valid():
-           user=form.save()
-           return redirect('user_creation.html')
-       else:
-           form=UsercreationForm()
-           return render(request,'user_creation.html',{'form':form})
+    if request.method == "POST":
+        form = UsercreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("user_creation")
+        else:
+            print(form.errors) 
+    else:
+        form = UsercreationForm()
 
-
+    return render(request, "user_creation.html", {"form": form})
         
 
 
